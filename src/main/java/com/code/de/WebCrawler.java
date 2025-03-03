bd36ad319d98387e1bf095b3c8763156b9ac2bb0
Nikolay Kudinov <nikolay.kudinov@zalando.de>
2025-03-03 10:31:52 +0100

    add crawl that have 1 control thread

--- src/main/java/com/code/de/WebCrawler.java
+++ src/main/java/com/code/de/WebCrawler.java
@@ -6,7 +6,12 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Set;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class WebCrawler {
 
@@ -16,7 +21,7 @@ public class WebCrawler {
     }
 
 
-    String getHome(String url) {
+    static String getHome(String url) {
         StringBuilder sb = new StringBuilder();
         int cnt = 0;
         for (char ch : url.toCharArray()) {
@@ -32,7 +37,7 @@ public class WebCrawler {
         return sb.toString();
     }
 
-    public List<String> crawl(String startUrl, HtmlParser htmlParser) {
+    public List<String> crawl1(String startUrl, HtmlParser htmlParser) {
         Queue<CompletableFuture<List<String>>> q = new LinkedList<>();
         Set<String> seen = new HashSet<>();
         seen.add(startUrl);
@@ -53,6 +58,36 @@ public class WebCrawler {
             }
         }
         return new ArrayList<>(seen);
+    }
+
+    public List<String> crawl(String startUrl, HtmlParser htmlParser) {
+        // each worker run code to get list of urls and put next batch in a queue
+        BlockingQueue<CompletableFuture<Void>> q = new LinkedBlockingQueue<>();
+        Set<String> seen = ConcurrentHashMap.newKeySet();
+        seen.add(startUrl);
+        String home = getHome(startUrl);
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        submit(home, startUrl, htmlParser, q, seen, executorService);
+        CompletableFuture<Void> cur;
+        while ((cur = q.poll()) != null) {
+            cur.join();
+        }
+        executorService.shutdown();
+        return new ArrayList<>(seen);
+    }
+
+    private static void submit(String home, String startUrl, HtmlParser htmlParser,
+        BlockingQueue<CompletableFuture<Void>> q,
+        Set<String> seen, ExecutorService executorService) {
+        q.add(CompletableFuture.runAsync(() -> {
+            for (String nextUrl : htmlParser.getUrls(startUrl)) {
+                if (home.equals(getHome(nextUrl))) {
+                    if (seen.add(nextUrl)) {
+                        submit(home, nextUrl, htmlParser, q, seen, executorService);
+                    }
+                }
+            },executorService));
+        };
     }
 }
 
