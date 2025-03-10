1dd1f2286e392f2a13e22ec6bf84da7bdf2707a0
Nikolay Kudinov <nikolay.a.kudinov@gmail.com>
2025-03-10 12:03:53 +0100

    add new version

--- src/main/java/com/code/de/Singleton.java
+++ src/main/java/com/code/de/Singleton.java
@@ -2,6 +2,21 @@ package com.code.de;
 
 public class Singleton {
 
+    private Singleton() {
+    }
+
+    private static volatile Singleton singleton;
+
+    public static Singleton getInstance() {
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
 
 
 }
