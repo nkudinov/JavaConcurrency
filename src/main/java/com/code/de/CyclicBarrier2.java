e95a88a72defde0a89b9bd92b2c395eee2f3a1cc
Nikolay Kudinov <nikolay.a.kudinov@gmail.com>
2025-03-31 14:12:14 +0200

    some changes

---
+++ src/main/java/com/code/de/CyclicBarrier2.java
@@ -0,0 +1,17 @@
+package com.code.de;
+
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicBarrier2 {
+    private int count;
+    Lock lock;
+
+    public CyclicBarrier2(int count) {
+        this.count = count;
+    }
+
+    public void await(){
+
+    }
+
+}
--- src/main/java/com/code/de/Singleton3.java
+++ src/main/java/com/code/de/Singleton3.java
@@ -1,16 +1,18 @@
 package com.code.de;
 
+
 public class Singleton3 {
 
     private static volatile Singleton3 instance;
 
-    private Singleton3() {
 
+    private Singleton3() {
     }
 
     public static Singleton3 getInstance() {
         if (instance == null) {
             synchronized (Singleton3.class) {
+                // Double-checked locking to ensure only one instance is created
                 if (instance == null) {
                     instance = new Singleton3();
                 }
@@ -19,3 +21,4 @@ public class Singleton3 {
         return instance;
     }
 }
+
