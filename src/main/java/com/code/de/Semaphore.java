90a68e7685eab41240955b4ae92af56befac7249
Nikolay Kudinov <nikolay.a.kudinov@gmail.com>
2025-04-07 07:58:00 +0200

    add Semaphore

---
+++ src/main/java/com/code/de/Semaphore.java
@@ -0,0 +1,24 @@
+package com.code.de;
+
+public class Semaphore {
+
+    private int permits;
+
+    public Semaphore(int permits) {
+        this.permits = permits;
+    }
+
+
+
+    public synchronized void acquire() throws InterruptedException {
+        while (permits == 0) {
+            wait();
+        }
+        permits--;
+    }
+
+    public synchronized void release() {
+         permits++;
+         notify();
+    }
+}
