3d69b2d795bbdc4db176b1487cc52ac3087a63fe
Nikolay Kudinov <nikolay.a.kudinov@gmail.com>
2025-04-07 13:31:04 +0200

    add

--- src/main/java/com/code/de/SimpleCyclicBarrier.java
+++ src/main/java/com/code/de/SimpleCyclicBarrier.java
@@ -1,5 +1,35 @@
 package com.code.de;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class SimpleCyclicBarrier {
 
+    private int count;
+    private final int initialCount;
+    private final Lock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition();
+
+    public SimpleCyclicBarrier(int count) {
+        this.count = count;
+        this.initialCount = count;
+    }
+
+    public void await() throws InterruptedException {
+        lock.lock();
+        try {
+            count--;
+            if (count == 0) {
+                count = initialCount; // Reset for next round
+                condition.signalAll(); // Release all waiting threads
+            } else {
+                while (count != initialCount) {
+                    condition.await(); // Wait until the barrier is tripped
+                }
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
 }
