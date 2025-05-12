baf24227e0ce62b9bbcb2659042389ae9e933e2c
Nikolay Kudinov <nikolay.a.kudinov@gmail.com>
2025-05-12 07:23:44 +0200

    add simple SynchronousQueue

--- src/main/java/com/code/de/WebCrawler.java
+++ src/main/java/com/code/de/WebCrawler.java
@@ -34,68 +34,10 @@ public class WebCrawler {
         return sb.toString();
     }
 
-    public List<String> crawl1(String startUrl, HtmlParser htmlParser) {
-        Queue<CompletableFuture<List<String>>> queue = new LinkedList<>();
-        String home = getHome(startUrl);
-        Set<String> seen = new HashSet<>();
-        seen.add(startUrl);
-        queue.add(CompletableFuture.supplyAsync(() -> htmlParser.getUrls(startUrl)));
-        while (!queue.isEmpty()) {
-            CompletableFuture<List<String>> cur = queue.poll();
-            try {
-                List<String> urls = cur.get();
-                for (String next : urls) {
-                    if (home.equals(getHome(next)) && seen.add(next)) {
-                        queue.add(CompletableFuture.supplyAsync(() -> htmlParser.getUrls(next)));
-                    }
-                }
-            } catch (Exception e) {
-                throw new RuntimeException("There was an issue");
-            }
-        }
-        return new ArrayList<>(seen);
-    }
 
     public List<String> crawl3(String startUrl, HtmlParser htmlParser) {
-        String home = getHome(startUrl);
-        BlockingQueue<String> queue = new LinkedBlockingQueue<>();
-        Set<String> seen = ConcurrentHashMap.newKeySet();
-        queue.add(startUrl);
-        seen.add(startUrl);
-        AtomicInteger counter = new AtomicInteger(1);
-        List<Thread> workers = new ArrayList<>();
-        for (int i = 1; i <= 10; i++) {
-            Thread worker = new Thread() {
-                @Override
-                public void run() {
-                    while (counter.get() != 0) {
-                        String url = queue.poll();
-                        if (url != null) {
-                            for (String nextUrl : htmlParser.getUrls(url)) {
-                                if (home.equals(getHome(nextUrl)) && seen.add(nextUrl)) {
-                                    counter.incrementAndGet();
-                                    queue.add(nextUrl);
-                                }
-                            }
-                            counter.decrementAndGet();
-                        }
-                    }
-                }
-            };
-            worker.start();
-            workers.add(worker);
-        }
-        for (Thread worker : workers) {
-            try {
-                worker.join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return new ArrayList<>(seen);
+
     }
-
-
 
 }
 
