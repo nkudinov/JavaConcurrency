6db770f9ebf2eef648ac61d8951a6627c0154b24
Nikolay Kudinov <nikolay.a.kudinov@gmail.com>
2025-06-02 14:58:32 +0200

    implement CyclicBarrier10

--- src/main/java/com/code/de/CyclicBarrier10.java
+++ src/main/java/com/code/de/CyclicBarrier10.java
@@ -1,7 +1,45 @@
 package com.code.de;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class CyclicBarrier10 {
 
+    private final int parties;
+    private int count;
+    private int generation = 0;
 
+    private final Lock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition();
 
+    public CyclicBarrier10(int parties) {
+        if (parties <= 0) {
+            throw new IllegalArgumentException("Barrier parties must be > 0");
+        }
+        this.parties = parties;
+        this.count = parties;
+    }
+
+    public void await() throws InterruptedException {
+        lock.lock();
+        try {
+            int currentGeneration = generation;
+            count--;
+
+            if (count == 0) {
+                // Last thread to arrive
+                generation++;
+                count = parties;
+                condition.signalAll(); // Wake up everyone
+                return;
+            }
+
+            while (generation == currentGeneration) {
+                condition.await();
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
 }
