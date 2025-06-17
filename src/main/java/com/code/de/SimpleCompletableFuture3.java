2990d0e8fdd9d670867fc597421493cad37ffa6b
Nikolay Kudinov <nikolay.a.kudinov@gmail.com>
2025-06-17 13:56:11 +0200

    implement SimpleCompletableFuture3

--- src/main/java/com/code/de/SimpleCompletableFuture3.java
+++ src/main/java/com/code/de/SimpleCompletableFuture3.java
@@ -11,8 +11,9 @@ public class SimpleCompletableFuture3<T> {
 
     private boolean hasCompleted = false;
     private T value;
-    private Lock lock = new ReentrantLock();
-    private Condition condition = lock.newCondition();
+    private final Lock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition();
+    private Runnable completionCallback;
 
     public SimpleCompletableFuture3() {
     }
@@ -25,20 +26,27 @@ public class SimpleCompletableFuture3<T> {
     public static <V> SimpleCompletableFuture3<V> supplyAsync(Callable<V> callable) {
         SimpleCompletableFuture3<V> res = new SimpleCompletableFuture3<>();
         Thread t = new Thread(() -> {
+            V result;
             try {
-                res.lock.lock();
-                res.hasCompleted = true;
-                res.value = callable.call();
-                res.condition.signalAll();
+                result = callable.call(); // Run outside lock
             } catch (Exception e) {
                 throw new RuntimeException(e);
+            }
+
+            res.lock.lock();
+            try {
+                res.value = result;
+                res.hasCompleted = true;
+                res.condition.signalAll();
+                if (res.completionCallback != null) {
+                    res.completionCallback.run();
+                }
             } finally {
                 res.lock.unlock();
             }
         });
         t.start();
         return res;
-
     }
 
     public T get() {
@@ -54,15 +62,26 @@ public class SimpleCompletableFuture3<T> {
     }
 
     public void whenComplete(Runnable runnable) {
-
+        lock.lock();
+        try {
+            if (hasCompleted) {
+                runnable.run();
+            } else {
+                completionCallback = runnable;
+            }
+        } finally {
+            lock.unlock();
+        }
     }
-
-    ;
 
     public static void main(String[] args) {
         var res = SimpleCompletableFuture3.supplyAsync(() -> {
+            Thread.sleep(100); // simulate some delay
             return "hello";
         });
+
+        res.whenComplete(() -> System.out.println("Completed!"));
+
         System.out.println(res.get());
     }
 }
