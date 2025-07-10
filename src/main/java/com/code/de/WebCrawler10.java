dc68b8ddc2e02e3ce202288703336438aefb9cee
nikolay-kudinov_zse <nikolay.a.kudinov@gmail.com>
2025-07-10 15:31:07 +0200

    implement Crawler9

---
+++ src/main/java/com/code/de/WebCrawler10.java
@@ -0,0 +1,11 @@
+package com.code.de;
+
+import java.util.List;
+
+public class WebCrawler10 implements WebCrawlerInterface {
+
+    public List<String> crawl(String startUrl, HtmlParser htmlParser) {
+        return List.of();
+    }
+    // dfs.
+}
